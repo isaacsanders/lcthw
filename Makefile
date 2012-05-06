0e00c368c97835e5576c6ddd75b83489bdec7365
Isaac Sanders <isanders@groupon.com>
2012-05-05 21:27:18 -0500

    Complete Exercise 3 (printf)

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CFLAGS=-Wall -g
 
-all: ex1
+all: ex1 ex3
 
 .DEFAULT: all
 
 clean:
-	rm -f ex1
+	rm -f ex1 ex3
---
+++ ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int age = 18;
+  int height = 75;
+
+  printf("I am %d years old.\n", age);
+  printf("I am %d inces tall.\n", height);
+
+  return 0;
+}
